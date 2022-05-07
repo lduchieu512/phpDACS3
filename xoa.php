33938f6f23db11e541fce4b1b4d302819f66ef50
lduchieu512 <lduchieu512@gmail.com>
2022-05-07 10:41:14 +0700

    video42

---
+++ xoa.php
@@ -0,0 +1,24 @@
+<?php
+ include "connect.php";
+$id = $_POST['id'];
+$query = 'DELETE FROM `sanphammoi` WHERE `id`='.$id;
+$data = mysqli_query($conn, $query);
+
+if ($data == true) {
+	$arr = [
+		'success' => true,
+		'message' => "Thành công",
+	];
+}else{
+	$arr = [
+		'success' => false,
+		'message' => "Xoá không thành công",
+
+	];
+}
+
+
+
+print_r(json_encode($arr));
+
+?>
